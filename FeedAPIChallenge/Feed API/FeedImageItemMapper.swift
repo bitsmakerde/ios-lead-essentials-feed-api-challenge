6422b2b359fc971140c9a90821bd6d1282f7563c
Andre Bongartz <info@bitsmaker.de>
2021-09-09 15:47:22 +0500

    add logic for checking invalid JSON add struct for decoupling API struct and RemoteFeedImage struct in FeedImageItemMapper

---
+++ FeedAPIChallenge/Feed API/FeedImageItemMapper.swift
@@ -0,0 +1,22 @@
+//
+//  FeedImageMapper.swift
+//  FeedAPIChallenge
+//
+//  Created by André Bongartz on 09.09.21.
+//  Copyright © 2021 Essential Developer Ltd. All rights reserved.
+//
+
+import Foundation
+
+internal enum FeedImageItemMapper {
+	struct Item: Decodable {
+		var image_id: UUID
+		var image_desc: String?
+		var image_loc: String?
+		var image_url: URL
+
+		var item: FeedImage {
+			return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
+		}
+	}
+}
--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,8 +21,9 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url, completion: { result in
 			switch result {
-			case let .success((_, respone)):
-				if respone.statusCode != 200 {
+			case let .success((data, response)):
+				if response.statusCode == 200, let dataAsJson = try? JSONDecoder().decode(FeedImageItemMapper.Item.self, from: data) {
+				} else {
 					completion(.failure(Error.invalidData))
 				}
 			case .failure:
